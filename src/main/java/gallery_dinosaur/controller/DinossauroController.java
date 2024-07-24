aaa1c441137fad0ab2939be26a758695862d5e87
MaceiraDev <leo.maceira.santos@hotmail.com>
2024-07-24 13:20:26 -0300

    Cadastro de imagem de dino, exclusão de dino + imagem

--- src/main/java/gallery_dinosaur/controller/DinossauroController.java
+++ src/main/java/gallery_dinosaur/controller/DinossauroController.java
@@ -4,24 +4,19 @@ import gallery_dinosaur.DTO.DinossauroRequestDTO;
 import gallery_dinosaur.DTO.DinossauroResponseDTO;
 import gallery_dinosaur.model.*;
 import gallery_dinosaur.repository.*;
-import jakarta.persistence.EntityNotFoundException;
-import jakarta.validation.Valid;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
+import javax.validation.Valid;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("dinossauro")
 public class DinossauroController {
-    private static final Logger LOGGER = Logger.getLogger(DinossauroController.class.getName());
 
     @Autowired
     DinossauroRepository repository;
@@ -59,118 +54,109 @@ public class DinossauroController {
     @Autowired
     SubFamiliaRepository subFamiliaRepository;
 
+    @Autowired
+    private ImageRepository imageRepository;
+
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
     public List<DinossauroResponseDTO> getAll() {
-        List<DinossauroResponseDTO> dinoList = repository.findAll().stream().map(DinossauroResponseDTO::new).toList();
-        return dinoList;
+        return repository.findAll().stream()
+                .map(DinossauroResponseDTO::new)
+                .toList();
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/{id}")
     public ResponseEntity<DinossauroResponseDTO> getById(@PathVariable Long id) {
-        DinossauroResponseDTO dino = repository.findById(id)
-                .map(DinossauroResponseDTO::new)
-                .orElseThrow(() -> new EntityNotFoundException("Dinossauro não encontrado neste ID: " + id));
-        return ResponseEntity.ok(dino);
+        Optional<Dinossauro> dinossauroOptional = repository.findById(id);
+        if (dinossauroOptional.isPresent()) {
+            DinossauroResponseDTO dino = new DinossauroResponseDTO(dinossauroOptional.get());
+            return ResponseEntity.ok(dino);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(null);
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping("/salvar")
-    public ResponseEntity<GlobalExceptionHandler.MessageResponse> salvarDinossauro(@Valid @RequestBody DinossauroRequestDTO data) {
-        Clado clado = cladoRepository.findById(data.getCladoId()).orElseThrow(() -> new EntityNotFoundException("Clado não encontrado"));
-        Dieta dieta = dietaRepository.findById(data.getDietaId()).orElseThrow(() -> new EntityNotFoundException("Dieta não encontrada"));
-        Dominio dominio = dominioRepository.findById(data.getDominioId()).orElseThrow(() -> new EntityNotFoundException("Dominio não encontrado"));
-        Especie especie = especieRepository.findById(data.getEspecieId()).orElseThrow(() -> new EntityNotFoundException("Especie não encontrada"));
-        Familia familia = familiaRepository.findById(data.getFamiliaId()).orElseThrow(() -> new EntityNotFoundException("Familia não encontrada"));
-        Filo filo = filoRepository.findById(data.getFiloId()).orElseThrow(() -> new EntityNotFoundException("Filo não encontrado"));
-        Genero genero = generoRepository.findById(data.getGeneroId()).orElseThrow(() -> new EntityNotFoundException("Genero não encontrado"));
-        MetodoLocomocao metodoLocomocao = metodoLocomocaoRepository.findById(data.getMetodoLocomocaoId()).orElseThrow(() -> new EntityNotFoundException("Metodo de Locomocao não encontrado"));
-        Periodo periodo = periodoRepository.findById(data.getPeriodoId()).orElseThrow(() -> new EntityNotFoundException("Periodo não encontrado"));
-        Reino reino = reinoRepository.findById(data.getReinoId()).orElseThrow(() -> new EntityNotFoundException("Reino não encontrado"));
-        SubFamilia subFamilia = subFamiliaRepository.findById(data.getSubFamiliaId()).orElseThrow(() -> new EntityNotFoundException("SubFamilia não encontrada"));
+    public ResponseEntity<DinossauroResponseDTO> salvarDinossauro(@Valid @RequestBody DinossauroRequestDTO data) {
+        Clado clado = cladoRepository.findById(data.getCladoId()).orElse(null);
+        Dieta dieta = dietaRepository.findById(data.getDietaId()).orElse(null);
+        Dominio dominio = dominioRepository.findById(data.getDominioId()).orElse(null);
+        Especie especie = especieRepository.findById(data.getEspecieId()).orElse(null);
+        Familia familia = familiaRepository.findById(data.getFamiliaId()).orElse(null);
+        Filo filo = filoRepository.findById(data.getFiloId()).orElse(null);
+        Genero genero = generoRepository.findById(data.getGeneroId()).orElse(null);
+        MetodoLocomocao metodoLocomocao = metodoLocomocaoRepository.findById(data.getMetodoLocomocaoId()).orElse(null);
+        Periodo periodo = periodoRepository.findById(data.getPeriodoId()).orElse(null);
+        Reino reino = reinoRepository.findById(data.getReinoId()).orElse(null);
+        SubFamilia subFamilia = subFamiliaRepository.findById(data.getSubFamiliaId()).orElse(null);
+
+        if (clado == null || dieta == null || dominio == null || especie == null || familia == null ||
+                filo == null || genero == null || metodoLocomocao == null || periodo == null || reino == null || subFamilia == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
 
         Dinossauro dinossauroData = new Dinossauro(data, clado, dieta, dominio, especie, familia, filo, genero, metodoLocomocao, periodo, reino, subFamilia);
-        repository.save(dinossauroData);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new GlobalExceptionHandler.MessageResponse("Dinossauro criado com sucesso!"));
+        Dinossauro savedDinossauro = repository.save(dinossauroData);
+
+        DinossauroResponseDTO responseDTO = new DinossauroResponseDTO(savedDinossauro);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @DeleteMapping("/deletar/{id}")
-    public ResponseEntity<GlobalExceptionHandler.MessageResponse> deletarDinossauro(@PathVariable Long id) {
+    @Transactional
+    public ResponseEntity<String> deletarDinossauro(@PathVariable Long id) {
         if (id == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new GlobalExceptionHandler.MessageResponse("ID não pode ser nulo."));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ID não pode ser nulo.");
         }
+
         Optional<Dinossauro> dinossauroOptional = repository.findById(id);
         if (dinossauroOptional.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new GlobalExceptionHandler.MessageResponse("Dinossauro não encontrado para o ID: " + id));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Dinossauro não encontrado para o ID: " + id);
         }
+
+        List<Image> images = imageRepository.findByDinossauroId(id);
+        if (!images.isEmpty()) {
+            System.out.println("Imagens associadas encontradas: " + images);
+        } else {
+            System.out.println("Nenhuma imagem associada encontrada.");
+        }
+
+        imageRepository.deleteByDinossauroId(id);
         repository.deleteById(id);
-        return ResponseEntity.status(HttpStatus.OK).body(new GlobalExceptionHandler.MessageResponse("Dinossauro do ID: " + id + " deletado com sucesso!"));
+
+        return ResponseEntity.status(HttpStatus.OK).body("Dinossauro do ID: " + id + " deletado com sucesso!");
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PutMapping("/atualizar/{id}")
-    public ResponseEntity<GlobalExceptionHandler.MessageResponse> atualizarDinossauro(@PathVariable Long id, @Valid @RequestBody DinossauroRequestDTO data) {
+    public ResponseEntity<String> atualizarDinossauro(@PathVariable Long id, @Valid @RequestBody DinossauroRequestDTO data) {
         Optional<Dinossauro> dinossauroOptional = repository.findById(id);
         if (dinossauroOptional.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new GlobalExceptionHandler.MessageResponse("Dinossauro não encontrado para o ID: " + id));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Dinossauro não encontrado para o ID: " + id);
         }
+
         Dinossauro dinossauro = dinossauroOptional.get();
         dinossauro.setNome(data.getNome());
         dinossauro.setTamanho(data.getTamanho());
         dinossauro.setPeso(data.getPeso());
         dinossauro.setDietaPrincipal(data.getDietaPrincipal());
         dinossauro.setHabitatNatural(data.getHabitatNatural());
-        dinossauro.setClado(cladoRepository.findById(data.getCladoId()).orElseThrow(() -> new EntityNotFoundException("Clado não encontrado")));
-        dinossauro.setDieta(dietaRepository.findById(data.getDietaId()).orElseThrow(() -> new EntityNotFoundException("Dieta não encontrada")));
-        dinossauro.setDominio(dominioRepository.findById(data.getDominioId()).orElseThrow(() -> new EntityNotFoundException("Dominio não encontrado")));
-        dinossauro.setEspecie(especieRepository.findById(data.getEspecieId()).orElseThrow(() -> new EntityNotFoundException("Especie não encontrada")));
-        dinossauro.setFamilia(familiaRepository.findById(data.getFamiliaId()).orElseThrow(() -> new EntityNotFoundException("Familia não encontrada")));
-        dinossauro.setFilo(filoRepository.findById(data.getFiloId()).orElseThrow(() -> new EntityNotFoundException("Filo não encontrado")));
-        dinossauro.setGenero(generoRepository.findById(data.getGeneroId()).orElseThrow(() -> new EntityNotFoundException("Genero não encontrado")));
-        dinossauro.setMetodoLocomocao(metodoLocomocaoRepository.findById(data.getMetodoLocomocaoId()).orElseThrow(() -> new EntityNotFoundException("Metodo de Locomocao não encontrado")));
-        dinossauro.setPeriodo(periodoRepository.findById(data.getPeriodoId()).orElseThrow(() -> new EntityNotFoundException("Periodo não encontrado")));
-        dinossauro.setReino(reinoRepository.findById(data.getReinoId()).orElseThrow(() -> new EntityNotFoundException("Reino não encontrado")));
-        dinossauro.setSubFamilia(subFamiliaRepository.findById(data.getSubFamiliaId()).orElseThrow(() -> new EntityNotFoundException("SubFamilia não encontrada")));
+        dinossauro.setClado(cladoRepository.findById(data.getCladoId()).orElse(null));
+        dinossauro.setDieta(dietaRepository.findById(data.getDietaId()).orElse(null));
+        dinossauro.setDominio(dominioRepository.findById(data.getDominioId()).orElse(null));
+        dinossauro.setEspecie(especieRepository.findById(data.getEspecieId()).orElse(null));
+        dinossauro.setFamilia(familiaRepository.findById(data.getFamiliaId()).orElse(null));
+        dinossauro.setFilo(filoRepository.findById(data.getFiloId()).orElse(null));
+        dinossauro.setGenero(generoRepository.findById(data.getGeneroId()).orElse(null));
+        dinossauro.setMetodoLocomocao(metodoLocomocaoRepository.findById(data.getMetodoLocomocaoId()).orElse(null));
+        dinossauro.setPeriodo(periodoRepository.findById(data.getPeriodoId()).orElse(null));
+        dinossauro.setReino(reinoRepository.findById(data.getReinoId()).orElse(null));
+        dinossauro.setSubFamilia(subFamiliaRepository.findById(data.getSubFamiliaId()).orElse(null));
+
         repository.save(dinossauro);
-        return ResponseEntity.status(HttpStatus.OK).body(new GlobalExceptionHandler.MessageResponse("Dinossauro do ID: " + id + " atualizada com sucesso!"));
-    }
-
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<GlobalExceptionHandler.MessageResponse> handleEntityNotFoundException(EntityNotFoundException ex) {
-        LOGGER.severe("Entity not found: " + ex.getMessage());
-        return new ResponseEntity<>(new GlobalExceptionHandler.MessageResponse(ex.getMessage()), HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<GlobalExceptionHandler.MessageResponse> handleGlobalException(Exception ex) {
-        LOGGER.severe("Error occurred: " + ex.getMessage());
-        return new ResponseEntity<>(new GlobalExceptionHandler.MessageResponse("Ouve algum erro, reclame com o caetano. https://www.facebook.com/photo/?fbid=602140598682206&set=a.261967682699501&locale=pt_BR"), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    static class GlobalExceptionHandler {
-        static class MessageResponse {
-            private String message;
-
-            public MessageResponse(String message) {
-                this.message = message;
-            }
-
-            public String getMessage() {
-                return message;
-            }
-
-            public void setMessage(String message) {
-                this.message = message;
-            }
-        }
+        return ResponseEntity.status(HttpStatus.OK).body("Dinossauro do ID: " + id + " atualizado com sucesso!");
     }
 }
--- src/main/java/gallery_dinosaur/controller/ImageController.java
+++ src/main/java/gallery_dinosaur/controller/ImageController.java
@@ -7,8 +7,16 @@ import gallery_dinosaur.repository.DinossauroRepository;
 import gallery_dinosaur.repository.ImageRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,24 +30,41 @@ public class ImageController {
     @Autowired
     private DinossauroRepository dinossauroRepository;
 
+    // Definindo o diretório de upload(onde será salvo as imagens)
+    private static final String UPLOAD_DIR = Paths.get(System.getProperty("user.dir"), "uploads").toString();
+
+    // Verificar e criar o diretório de upload, se necessário
+    static {
+        File uploadDir = new File(UPLOAD_DIR);
+        if (!uploadDir.exists()) {
+            uploadDir.mkdirs();
+        }
+    }
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping("/save")
-    public List<ImageRequestDTO> save(@RequestBody List<ImageRequestDTO> data) {
-        List<Image> images = data.stream().map(dto -> {
-            Dinossauro dinossauro = dinossauroRepository.findById(dto.getDinossauroId())
-                    .orElseThrow(() -> new EntityNotFoundException("Dinossauro não encontrado para o ID: " + dto.getDinossauroId()));
+    public ResponseEntity<List<ImageRequestDTO>> save(@RequestParam("file") MultipartFile file, @RequestParam("dinossauroId") Long dinossauroId) {
+        try {
+            Dinossauro dinossauro = dinossauroRepository.findById(dinossauroId)
+                    .orElseThrow(() -> new EntityNotFoundException("Dinossauro não encontrado para o ID: " + dinossauroId));
+
+            // Salvar o arquivo localmente
+            String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+            Path filePath = Paths.get(UPLOAD_DIR, fileName);
+            Files.write(filePath, file.getBytes());
+
+            // Salvar a URL da imagem no banco de dados
             Image image = new Image();
-            image.setUrl(dto.getUrl());
+            image.setUrl(filePath.toString());
             image.setDinossauro(dinossauro);
-            return image;
-        }).collect(Collectors.toList());
+            image = imageRepository.save(image);
 
-        images = imageRepository.saveAll(images);
-
-        return images.stream().map(image -> {
             ImageRequestDTO dto = new ImageRequestDTO();
             dto.setUrl(image.getUrl());
             dto.setDinossauroId(image.getDinossauro().getId());
-            return dto;
-        }).collect(Collectors.toList());
+
+            return new ResponseEntity<>(List.of(dto), HttpStatus.OK);
+        } catch (IOException e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
